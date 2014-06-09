5cd90a54de5efff0297ddeb2b99c6ea09d24e788
Matt Loppatto <mattloppatto@gmail.com>
2014-06-09 22:06:30 +0000

    dont remember

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 cd ~/poker/test
+
 node shuffle.js
+#if [ $? -gt 0 ]; then
+#    echo "[x] shuffle.js"
+#    exit $?
+#fi
+#echo -e "[\xE2\x9C\x93] shuffle.js"
+
+node evaluator.js
+#if [ $? -gt 0 ]; then
+#    echo "[x] shuffle.js"
+#    exit $?
+#fi
+#echo -e "[\xE2\x9C\x93] evaluator.js"
+
+#echo "All tests passed!"
 
